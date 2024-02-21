fcf8a4258c7f840666c9c55c1cce561f6e3b683b
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-02-21 11:46:43 +0100

    models + validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,16 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES,  message: "%{value} is not a valid category"  }
+
+end
+
+# rails generate model restaurant name:string address:string phone_number:string category:string
+
+# validation reqs
+# [X] A restaurant must have a name, an address and a category.
+# [X] A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# [X] When a restaurant is destroyed, all of its reviews must be destroyed as well.
---
+++ app/models/review.rb
@@ -0,0 +1,18 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { in: 0..5 }
+  validates :restaurant, presence: true
+
+end
+
+# rails generate model review content:text rating:integer restaurant:references
+
+# validation reqs
+# [X] A review must have a content.
+# [X] A review must have a rating.
+# [X] A review’s rating must be a number between 0 and 5.
+# [X] A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
+# [X] A review must belong to a restaurant.
